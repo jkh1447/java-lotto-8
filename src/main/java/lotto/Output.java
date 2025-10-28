98b29f8aea92ff2765b659490eed7ff2a60bab91
jkh1447 <jkh1447@gmail.com>
2025-10-28 19:11:54 +0900

    feat(output): 결과 및 안내문 출력
    구입금액에 대한 안내문 출력.
    구입금액에 따른 로또 구매 개수 출력.
    당첨 번호 입력에 대한 안내문 출력.
    보너스 번호 입력에 대한 안내문 출력.

---
+++ src/main/java/lotto/Output.java
@@ -0,0 +1,25 @@
+package lotto;
+
+public class Output {
+
+    public static final String INPUT_AMOUNT_OF_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String LOTTO_BUY_MESSAGE = "%d개를 구매했습니다.";
+    public static final String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUMBERS_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    public void printInputMoney() {
+        System.out.println(INPUT_AMOUNT_OF_MONEY_MESSAGE);
+    }
+
+    public void printInputWinningNumbers() {
+        System.out.println(INPUT_WINNING_NUMBERS_MESSAGE);
+    }
+
+    public void printInputBonusNumbers() {
+        System.out.println(INPUT_BONUS_NUMBERS_MESSAGE);
+    }
+
+    public void printBuyMessage(int n) {
+        System.out.printf(LOTTO_BUY_MESSAGE, n);
+    }
+}
