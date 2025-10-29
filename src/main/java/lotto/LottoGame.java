54d8574e52286d5e9aab37376431c6e3fe34a845
jkh1447 <jkh1447@gmail.com>
2025-10-29 16:18:04 +0900

    feat(output): 출력
    로또 발행수 출력.
    발행한 로또번호 출력

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,16 +1,20 @@
 package lotto;
 
+import java.util.List;
+
 public class LottoGame {
 
     Input input = new Input();
     Output output = new Output();
+    LottoGenerator lottoGenerator = new LottoGenerator();
 
     private void run() {
         output.printInputMoney();
         String moneyStr = input.InputMoney();
         int money = Parsing.parsingMoney(moneyStr);
 
-
-        
+        List<Lotto> lottos = lottoGenerator.generateLotto(money);
+        output.printBuyMessage(lottos.size());
+        output.printLottos(lottos);
     }
 }
--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -1,11 +1,14 @@
 package lotto;
 
+import java.util.List;
+
 public class Output {
 
     public static final String INPUT_AMOUNT_OF_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
     public static final String LOTTO_BUY_MESSAGE = "%d개를 구매했습니다.";
     public static final String INPUT_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
     public static final String INPUT_BONUS_NUMBERS_MESSAGE = "보너스 번호를 입력해 주세요.";
+
 
     public void printInputMoney() {
         System.out.println(INPUT_AMOUNT_OF_MONEY_MESSAGE);
@@ -21,5 +24,11 @@ public class Output {
 
     public void printBuyMessage(int n) {
         System.out.printf(LOTTO_BUY_MESSAGE, n);
+    }
+
+    public void printLottos(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
     }
 }
