9c475e47b7fe916fe1a843e52fc8c3742709c9bc
jkh1447 <jkh1447@gmail.com>
2025-10-29 16:42:36 +0900

    feat(LottoGame): 로또 게임
    예외처리시 입력으로 돌아가는 기능.
    로또 당첨번호와 보너스 번호 입력받기.
    구입금액에 따른 로또 발행.

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -10,11 +10,53 @@ public class LottoGame {
 
     private void run() {
         output.printInputMoney();
-        String moneyStr = input.InputMoney();
-        int money = Parsing.parsingMoney(moneyStr);
+        int money = readMoney();
 
         List<Lotto> lottos = lottoGenerator.generateLotto(money);
         output.printBuyMessage(lottos.size());
         output.printLottos(lottos);
+
+        output.printInputWinningNumbers();
+        List<Integer> winningNumbers = readWinningNumbers();
+
+        output.printInputBonusNumbers();
+        int bonus = readBonusNumber();
+
+        
+
+
+    }
+
+    public int readMoney() {
+        while(true) {
+            try{
+                String moneyStr = input.InputMoney();
+                int money = Parsing.parsingMoney(moneyStr);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public List<Integer> readWinningNumbers() {
+        while(true) {
+            try{
+                String winningStr = input.InputWinningNumbers();
+                return Parsing.parsingWinningNumbers(winningStr);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public int readBonusNumber() {
+        while(true) {
+            try{
+                String bonusStr = input.InputBonusNumbers();
+                return Parsing.parsingBonusNumber(bonusStr);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
